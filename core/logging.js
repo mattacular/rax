fa297e3220c400ec7ea720dae1723011c07d541c
Matt Stills <mattacular@gmail.com>
2012-12-19 02:14:28 -0500

    removing crap

--- core/logging.js
+++ core/logging.js
@@ -6,21 +6,22 @@ var Rax = global.rax;
 
 // global.rax.log = Logs.log;
 
-function check(msg, color) {
+function cLog(msg, color) {
 	if (Rax.cfg.ENABLE_LOGGING) {
 		console.log((msg)[color]);
 	}
 }
 
-Logs.g = function (msg) { check(msg, 'green'); }
-Logs.r = function (msg) { check(msg, 'red'); }
-Logs.y = function (msg) { check(msg, 'yellow'); }
-Logs.c = function (msg) { check(msg, 'cyan'); }
-Logs.w = function (msg) { check(msg, 'white'); }
-Logs.b = function (msg) { check(msg, 'blue'); }
-Logs.m = function (msg) { check(msg, 'magenta'); }
-Logs.em = function (msg) { check(msg, 'bold'); }
-Logs.u = function (msg) { check(msg, 'underline'); }
+// color logging shortcuts
+Logs.g = function (msg) { cLog(msg, 'green'); }
+Logs.r = function (msg) { cLog(msg, 'red'); }
+Logs.y = function (msg) { cLog(msg, 'yellow'); }
+Logs.c = function (msg) { cLog(msg, 'cyan'); }
+Logs.w = function (msg) { cLog(msg, 'white'); }
+Logs.b = function (msg) { cLog(msg, 'blue'); }
+Logs.m = function (msg) { cLog(msg, 'magenta'); }
+Logs.em = function (msg) { cLog(msg, 'bold'); }
+Logs.u = function (msg) { cLog(msg, 'underline'); }
 
 // note you can also prototype core module methods onto the top level app object this way
 Logs.log = global.rax.log = function () {
