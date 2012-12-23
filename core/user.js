78d8040cecedb2784d2412e8e265c7aeddfd5617
Matt Stills <mattacular@gmail.com>
2012-12-23 00:24:01 -0500

    try nodemailer

--- core/user.js
+++ core/user.js
@@ -5,7 +5,8 @@ var User = module.exports = {},
 	Rax = require('./rax'),
 	mongoose = require('mongoose'),
 	bcrypt = require('bcrypt'),			// encryption module for safe password persistence
-	SALT_WORK_FACTOR = 10;
+	SALT_WORK_FACTOR = 10,
+	nodemailer = require('nodemailer');
 
 User.routes = {
 	'/logout': {
@@ -25,6 +26,38 @@ User.routes = {
 		}
 	}
 };
+
+// Rax.beacon.on('init', function () {
+// 	// create reusable transport method (opens pool of SMTP connections)
+// 	var smtpTransport = nodemailer.createTransport("SMTP",{
+// 	    service: "Gmail",
+// 	    auth: {
+// 	        user: ""
+// 	        pass: ""
+// 	    }
+// 	});
+
+// 	// setup e-mail data with unicode symbols
+// 	var mailOptions = {
+// 	    from: "Rax <rax@gmail.com>", // sender address
+// 	    to: "mattacular@gmail.com", // list of receivers
+// 	    subject: "Hello, this is Rax", // Subject line
+// 	    text: "Wasup dog", // plaintext body
+// 	    html: "<b>Wasup doggy</b>" // html body
+// 	}
+
+// 	// send mail with defined transport object
+// 	smtpTransport.sendMail(mailOptions, function(error, response){
+// 	    if(error){
+// 	        console.log(error);
+// 	    }else{
+// 	        console.log("Message sent: " + response.message);
+// 	    }
+
+// 	    // if you don't want to use this transport object anymore, uncomment following line
+// 	    smtpTransport.close(); // shut down the connection pool, no more messages
+// 	});
+// });
 
 // User.db = provide schema and models
 
